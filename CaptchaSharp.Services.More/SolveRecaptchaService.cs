56dc2e835385c90a82b472bf45c5508dbd6b9d18
ruri <ruridev@protonmail.com>
2020-04-14 03:26:49 +0200

    Added SolveRecaptchaService

---
+++ CaptchaSharp.Services.More/SolveRecaptchaService.cs
@@ -0,0 +1,17 @@
+﻿using CaptchaSharp.Enums;
+using System;
+using System.Net.Http;
+
+namespace CaptchaSharp.Services.More
+{
+    public class SolveRecaptchaService : CustomTwoCaptchaService
+    {
+        public SolveRecaptchaService(string apiKey, HttpClient httpClient = null)
+            : base(apiKey, new Uri("http://api.solverecaptcha.com"), httpClient)
+        {
+            SupportedCaptchaTypes =
+                CaptchaType.ReCaptchaV2 |
+                CaptchaType.ReCaptchaV3;
+        }
+    }
+}
