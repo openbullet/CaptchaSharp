e62f4d781d47b09fd04ed454d6ecfe68353884cc
Ruri <ruridev@protonmail.com>
2021-06-27 01:48:37 +0200

    Added Time property to CaptchaResponse
    Closes #4

--- CaptchaSharp/Models/CaptchaResponse.cs
+++ CaptchaSharp/Models/CaptchaResponse.cs
@@ -1,9 +1,14 @@
-﻿namespace CaptchaSharp.Models
+﻿using System;
+
+namespace CaptchaSharp.Models
 {
     /// <summary>A generic captcha response.</summary>
     public class CaptchaResponse
     {
         /// <summary>The captcha id which is needed to report the solution as bad.</summary>
         public long Id { get; set; }
+
+        /// <summary>The time when the solution was received.</summary>
+        public DateTime Time { get; set; } = DateTime.Now;
     }
 }
