5b85b4fdbbdae176d52fb42b5d12d380fe55afb1
Ruri <ruridev@protonmail.com>
2022-01-02 14:51:44 +0100

    Added TrueCaptcha tests

---
+++ CaptchaSharp.Tests/TrueCaptchaTests.cs
@@ -0,0 +1,24 @@
+﻿using CaptchaSharp.Services.More;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CaptchaSharp.Tests
+{
+    public class TrueCaptchaFixture : ServiceFixture
+    {
+        public TrueCaptchaFixture()
+        {
+            Service = new TrueCaptchaService(
+                Config.Credentials.TrueCaptchaUsername,
+                Config.Credentials.TrueCaptchaApiKey);
+        }
+    }
+
+    public class TrueCaptchaServiceTests : ServiceTests, IClassFixture<TrueCaptchaFixture>
+    {
+        public TrueCaptchaServiceTests(TrueCaptchaFixture fixture) : base(fixture) { }
+
+        [Fact] public Task GetBalanceAsync_ValidKey_GetsBalance() => BalanceTest();
+        [Fact] public Task SolveImageCaptchaAsync_ValidCaptcha_ValidSolution() => ImageCaptchaTest();
+    }
+}
