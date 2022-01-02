3be73cbf694df4deac375603148999c6104114e9
Ruri <ruridev@protonmail.com>
2022-01-02 12:50:47 +0100

    Fixed test method names

--- CaptchaSharp.Tests/ImageTyperzServiceTests.cs
+++ CaptchaSharp.Tests/ImageTyperzServiceTests.cs
@@ -17,18 +17,18 @@ namespace CaptchaSharp.Tests
         public ImageTyperzServiceTests(ImageTyperzFixture fixture) : base(fixture) { }
 
         [Fact] public Task GetBalanceAsync_ValidKey_GetsBalance() => BalanceTest();
-        [Fact] public Task SolveTextCaptchaAsync_ValidCaptcha_ValidSolution() => ShouldNotBeSupported(TextCaptchaTest);
+        [Fact] public Task SolveTextCaptchaAsync_ValidCaptcha_NotSupported() => ShouldNotBeSupported(TextCaptchaTest);
         [Fact] public Task SolveImageCaptchaAsync_ValidCaptcha_ValidSolution() => ImageCaptchaTest();
         [Fact] public Task SolveRecaptchaV2Async_NoProxy_ValidSolution() => RecaptchaV2Test_NoProxy();
         [Fact] public Task SolveRecaptchaV2Async_WithProxy_ValidSolution() => RecaptchaV2Test_WithProxy();
         [Fact] public Task SolveRecaptchaV3Async_NoProxy_ValidSolution() => RecaptchaV3Test_NoProxy();
         [Fact] public Task SolveRecaptchaV3Async_WithProxy_ValidSolution() => RecaptchaV3Test_WithProxy();
-        [Fact] public Task SolveFuncaptchaAsync_NoProxy_ValidSolution() => ShouldNotBeSupported(FunCaptchaTest_NoProxy);
-        [Fact] public Task SolveFuncaptchaAsync_WithProxy_ValidSolution() => ShouldNotBeSupported(FunCaptchaTest_WithProxy);
+        [Fact] public Task SolveFuncaptchaAsync_NoProxy_NotSupported() => ShouldNotBeSupported(FunCaptchaTest_NoProxy);
+        [Fact] public Task SolveFuncaptchaAsync_WithProxy_NotSupported() => ShouldNotBeSupported(FunCaptchaTest_WithProxy);
         [Fact] public Task SolveHCaptchaAsync_NoProxy_ValidSolution() => HCaptchaTest_NoProxy();
         [Fact] public Task SolveHCaptchaAsync_WithProxy_ValidSolution() => HCaptchaTest_WithProxy();
-        [Fact] public Task SolveKeyCaptchaAsync_NoProxy_ValidSolution() => ShouldNotBeSupported(KeyCaptchaTest_NoProxy);
-        [Fact] public Task SolveKeyCaptchaAsync_WithProxy_ValidSolution() => ShouldNotBeSupported(KeyCaptchaTest_WithProxy);
+        [Fact] public Task SolveKeyCaptchaAsync_NoProxy_NotSupported() => ShouldNotBeSupported(KeyCaptchaTest_NoProxy);
+        [Fact] public Task SolveKeyCaptchaAsync_WithProxy_NotSupported() => ShouldNotBeSupported(KeyCaptchaTest_WithProxy);
         [Fact] public Task SolveGeeTestAsync_NoProxy_ValidSolution() => GeeTestTest_NoProxy();
         [Fact] public Task SolveGeeTestAsync_WithProxy_ValidSolution() => GeeTestTest_WithProxy();
         [Fact] public Task SolveCapyAsync_NoProxy_ValidSolution() => CapyTest_NoProxy();
